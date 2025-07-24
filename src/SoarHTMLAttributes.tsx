1dc72d54831b485228f64bc41d83ba6d6c897c17
veloz-web <adelorie@silveredge-gs.com>
2025-07-24 21:39:25 +0000

    test commit

--- src/SoarHTMLAttributes.tsx
+++ src/SoarHTMLAttributes.tsx
@@ -1,20 +1,19 @@
 import { Component, createElement } from "react";
 
-import { HTMLAttr } from "./components/HTMLAttr"
+import { HTMLAttr } from "./components/HTMLAttr";
 import { SoarHTMLAttributeContainerProps } from "../typings/SoarHTMLAttributeProps";
 
 class SoarHTMLAttribute extends Component<SoarHTMLAttributeContainerProps> {
-
     render() {
         return (
             <HTMLAttr
-            	name={this.props.selectorName}
-            	withContext={this.props.context}
-            	contextValue={this.props.contextAttribute ? this.props.contextAttribute.displayValue : ""}
-            	targetType={this.props.elemSelector}
-            	targetName={this.props.specificElement}
-            ></HTMLAttr>  
-        )
+                name={this.props.selectorName}
+                withContext={this.props.context}
+                contextValue={this.props.contextAttribute ? this.props.contextAttribute.displayValue : ""}
+                targetType={this.props.elemSelector}
+                targetName={this.props.specificElement}
+            ></HTMLAttr>
+        );
     }
 }
 
